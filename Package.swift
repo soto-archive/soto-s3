6d8d022205e5af64c7cfb5a83bb58cada4fac198
noppoman <yuki@miketokyo.com>
2017-10-08 23:19:08 +0900

    sync with aws-sdk-swift@1.0.1

--- Package.swift
+++ Package.swift
@@ -1,10 +1,16 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.0
 
 import PackageDescription
 
 let package = Package(
   name: "SwiftAWSS3",
+  products: [
+      .library(name: "SwiftAWSS3", targets: ["SwiftAWSS3"]),
+  ],
   dependencies: [
-      .Package(url: "https://github.com/noppoMan/aws-sdk-swift-core.git", majorVersion: 0, minor: 2)
+      .package(url: "https://github.com/noppoMan/aws-sdk-swift-core.git", .upToNextMajor(from: "1.0.0"))
+  ],
+  targets: [
+      .target(name: "SwiftAWSS3", dependencies: ["AWSSDKSwiftCore"]),
   ]
 )
