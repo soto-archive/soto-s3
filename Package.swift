584d076e23ad89050a52beb41a515649cfb2900f
Adam Fowler <adamfowler71@gmail.com>
2020-03-12 18:15:14 +0000

    Version 4.2.1

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
       .library(name: "S3", targets: ["S3"]),
   ],
   dependencies: [
-      .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .upToNextMinor(from: "4.0.0"))
+      .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .upToNextMinor(from: "4.2.0"))
   ],
   targets: [
     .target(name: "S3", dependencies: ["AWSSDKSwiftCore", "S3Middleware"]),
